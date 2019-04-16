72f9745cf2c0ded482fe1cc4cc981636b3dbd488
Munaf <munafhajirpro@gmail.com>
2019-04-16 17:39:54 +0530

    Algorithm for same location of Startup

--- server.py
+++ server.py
@@ -15,6 +15,15 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/apdc"
 mongo = PyMongo(app)
 pp = pprint.PrettyPrinter(indent=4)
 
+def startupRankLoc(location):
+    startup_dict = {}
+    # csv = open("START.csv","r","encoding = utf-8")
+    dataset = pd.read_csv("datasets/startup_ranking.csv",delimiter=',', names = ['rank', 'startup', 'startup_logo', 'sr_score', 'description', 'location','none'])
+    startupLoc = dataset[dataset.location == 'Canada'].head(10)
+    startupLoc = startupLoc.to_dict('index')
+    pp.pprint(startupLoc)
+    return startupLoc
+
 def read_csv(category):
     startup_dict = {}
     # csv = open("START.csv","r","encoding = utf-8")
